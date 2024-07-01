d528b02e82cc65a20d95d65db2e07c990564414e
Anasbarrak <barrak.mohamedanas@gmail.com>
2024-07-01 17:40:02 +0100

    fisrt commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import StopWatch from './Components/Stopwatch/Stopwatch';
+
+
+function App() {
+  return (    
+    <div className='App'>
+      <StopWatch />
+    </div>
+  );
+}
+
+export default App;
